263785992cc78dd8c801458e6243f101d86dfdb5
felipealvss <lipealvessax@gmail.com>
2019-07-08 09:47:57 -0300

    Working on, still learning...

---
+++ src/models/contact.model.js
@@ -0,0 +1,36 @@
+// contact-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const contact = new Schema({
+    
+    name: { 
+      first: {
+        type: String,
+        required: [true, 'First name is required'],
+      },
+      last: {
+        type: String,
+        required: false,
+      }
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone number is required']
+    }
+  },
+    {
+      timestamps: true
+    }
+  );
+
+  return mongooseClient.model('contact', contact);
+};
